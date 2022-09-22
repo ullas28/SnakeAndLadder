44d1af0e4bd57e37c467ca0778b670de745252e6
unknown <you@example.com>
2022-09-22 23:18:10 +0530

    Added Welcome to Snake and Ladder program

---
+++ Welcome.java
@@ -0,0 +1,9 @@
+/*Snake & Ladder Simulator*/
+
+package com.bridgelabz.com;
+
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Welcome to snake Ladder Program");
+    }
+}
