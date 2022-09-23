b0919b9c8522a960ee63ba3bf3d0611165fd0a70
unknown <you@example.com>
2022-09-23 07:59:28 +0530

    Added : Starting position of player

---
+++ StartPosition.java
@@ -0,0 +1,10 @@
+/*Snake and Ladder game played with single player at start position 0*/
+
+package com.bridgelabz.com;
+public class StartPosition {
+        public static void main(String[] args) {
+                int playerPosition = 0;
+                System.out.println("Position of player is " + playerPosition);
+        }
+}
+
